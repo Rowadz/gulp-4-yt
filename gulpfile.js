f5d956582c5d26e8cc7d47dc1c3216d39fb0f00f
MohammedAl-rowad <rowadwo@gmail.com>
2020-09-24 04:22:10 +0300

    browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass = require('gulp-sass')
 const { join } = require('path')
 const cleanCSS = require('gulp-clean-css')
 const rename = require('gulp-rename')
+const browserSync = require('browser-sync').create()
 require('colors')
 sass.compiler = require('node-sass')
 const path = join(__dirname, 'src')
@@ -35,7 +36,16 @@ const minifyJS = (cb) => {
   cb()
 }
 
+const dev = () => {
+  browserSync.init({
+    server: {
+      baseDir: join(path),
+    },
+  })
+}
+
 exports.default = series(
   parallel(compileJS, compileSCSS),
-  parallel(minifyCSS, minifyJS)
+  parallel(minifyCSS, minifyJS),
+  dev
 )
